e697b55ed27aee3f21b9b6d8732775b5902131cd
deepikadishadisha-rgb <deepikadishadisha@gmail.com>
2026-08-17 15:17:08 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,25 @@
+package deepika1;
+
+public class palindrome {
+
+	public static void main(String[] args) {
+		
+		        int num = 121;
+		        int rev = 0;
+		        int temp = num;
+
+		        while (num > 0) {
+		            rev = rev * 10 + num % 10;
+		            num = num / 10;
+		        }
+
+		        if (temp == rev)
+		            System.out.println("Palindrome");
+		        else
+		            System.out.println("Not Palindrome");
+		    }
+		
+
+	}
+
+
