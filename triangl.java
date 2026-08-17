0c26cd32e4b3551f88a9f3ee5832b680d1cba82f
deepikadishadisha-rgb <deepikadishadisha@gmail.com>
2026-08-17 16:46:05 +0530

    Add files via upload

---
+++ triangl.java
@@ -0,0 +1,18 @@
+package deepika1;
+
+public class triangl {
+
+	public static void main(String[] args) {
+		
+		        double base = 10;
+		        double height = 5;
+
+		        double area = 0.5 * base * height;
+
+		        System.out.println("Area = " + area);
+		    }
+		
+
+	}
+
+
