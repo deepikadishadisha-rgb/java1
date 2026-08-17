78a0191bf8855b382ce8b7816a7777fcba840e16
deepikadishadisha-rgb <deepikadishadisha@gmail.com>
2026-08-17 16:48:48 +0530

    Add files via upload

---
+++ circle1.java
@@ -0,0 +1,16 @@
+package deepika1;
+
+public class circle1 {
+
+	public static void main(String[] args) {
+		
+		        double r = 5;
+		        double area = 3.14 * r * r;
+
+		        System.out.println("Area = " + area);
+		    }
+		
+
+	}
+
+
