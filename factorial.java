0a187243b9ac2c50c5988dac027f925b4ebc462f
deepikadishadisha-rgb <deepikadishadisha@gmail.com>
2026-08-17 13:45:23 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,25 @@
+package deepika;
+
+public class factorial {
+public static void main(String[] args) {
+
+				        int a = 5, b = 3;
+				        int f1 = 1, f2 = 1;
+
+				        for(int i = 1; i <= a; i++)
+				            f1 *= i;
+
+				        for(int i = 1; i <= b; i++)
+				            f2 *= i;
+
+				        System.out.println(f1);
+				        System.out.println(f2);
+				    }
+				
+
+			
+
+
+	}
+
+
