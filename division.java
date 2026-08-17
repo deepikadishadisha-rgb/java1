e17c6aed6456cbe625f439dc052d21ff7f99b598
deepikadishadisha-rgb <deepikadishadisha@gmail.com>
2026-08-17 16:36:49 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,16 @@
+package deepika1;
+
+public class division {
+
+	public static void main(String[] args) {
+	
+		        int a = 20;
+		        int b = 5;
+
+		        System.out.println(a / b);
+		    }
+		
+
+	}
+
+
