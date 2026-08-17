6cf9c8df6f7d16d949979e3d7ce000c4e444c13f
deepikadishadisha-rgb <deepikadishadisha@gmail.com>
2026-08-17 16:31:28 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,16 @@
+package deepika1;
+
+public class multiply {
+
+	public static void main(String[] args) {
+		
+		        int a = 5;
+		        int b = 4;
+
+		        System.out.println(a * b);
+		    }
+		
+
+	}
+
+
